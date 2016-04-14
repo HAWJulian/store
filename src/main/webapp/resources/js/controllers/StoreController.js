74ac5a69b01b4ada59bf9a77b2f10b286852c670
HAWJulian <yuiky@web.de>
2016-04-14 08:07:46 +0200

    search function implemented

--- src/main/webapp/resources/js/controllers/StoreController.js
+++ src/main/webapp/resources/js/controllers/StoreController.js
@@ -2,28 +2,26 @@
  * Created by tmichels on 8/3/14.
  */
 
+var StoreController = function($scope, $http) {
 
-var StoreController = function($scope, $http){
+	$scope.items = [ {
+		name : 'Käse',
+		price : 1000,
+		make : 'Ja!',
+		needsCooling : true,
+		//image: "./images/3.png"
+	}, {
+		name : 'Wurst',
+		price : 10,
+		make : 'Nein?',
+		needsCooling : false,
+		//image: "./images/10.png"
+	}, {
+		name : 'Marmelade',
+		price : 2,
+		make : 'Schwartau',
+		needsCooling : true,
+		//image: "./images/21.png"
+	} ];
 
-	$scope.items = [
-	                {
-	                	name: 'Käse',
-	                	price: 1000,
-	                	make: 'Ja!',
-	                	needsCooling: true
-	                },
-	                {
-	                	name: 'Wurst',
-	                	price: 10,
-	                	make: 'Nein?',
-	                	needsCooling: false
-	                },
-	                {
-	                	name: 'Marmelade',
-	                	price: 2,
-	                	make: 'Schwartau',
-	                	needsCooling: true
-	                }
-	                ];
-    
 }
