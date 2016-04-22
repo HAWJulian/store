76e5fc7e1075e0aa466480cca0b7a62b03c29f39
HAWJulian <yuiky@web.de>
2016-04-22 14:57:42 +0200

    db connector, pom.xml preperation

--- src/main/java/com/cloudsole/angular/config/PostgresConfig.java
+++ src/main/java/com/cloudsole/angular/config/PostgresConfig.java
@@ -1,7 +1,30 @@
 package com.cloudsole.angular.config;
-
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+ 
+import org.apache.commons.dbcp.*;
+import org.springframework.context.annotation.Bean;
 /**
  * Created by tmichels on 8/4/14.
  */
-public class PostgresConfig {
+public class PostgresConfig
+{
+
+	@Bean
+	public BasicDataSource dataSource() throws URISyntaxException
+	{
+		String dbUrl = System.getenv("JDBC_DATABASE_URL");
+		String username = System.getenv("JDBC_DATABASE_USERNAME");
+		String password = System.getenv("JDBC_DATABASE_PASSWORD");
+
+		BasicDataSource basicDataSource = new BasicDataSource();
+		basicDataSource.setUrl(dbUrl);
+		basicDataSource.setUsername(username);
+		basicDataSource.setPassword(password);
+
+		return basicDataSource;
+	}
 }
